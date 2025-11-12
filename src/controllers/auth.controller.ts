d00d6ad20b0e25e7323a84ffaecdb629dd890dca
SmasBalloon <mathissauvage24000@gmail.com>
2025-11-12 13:19:35 +0000

    Corrige la logique de réponse dans la fonction de connexion pour retourner immédiatement après l'envoi du token ou du message d'erreur.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -44,15 +44,15 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
     if (isMatch) {
       const token = await generateToken(email)
-      res.status(200).json({ token })
+      return res.status(200).json({ token })
+    } else {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
   } catch (error) {
     res.status(401).json({ message: "Invalid credentials" });
     next(error);
   }
-
-  res.status(200).json({ message: "Login successful" });
 }
 
 
